6b065915bd6076794f8f70b1cb7e20c570049c6f
tsuyoshi wada <wadackel@gmail.com>
2021-09-03 18:05:07 +0900

    fix: tweak

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -18,7 +18,7 @@ echo ${PUBLISH_VERSIONS}
 for v in $PUBLISH_VERSIONS
 do
   # nothing Docker image...
-  if [[ $v = "13.9.0" ]]; then
+  if [[ $v = "13.9.0" || $v = "13.10.0" ]]; then
     continue
   fi
 
